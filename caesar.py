b331c99c75f2c4af5d97fd46b9267f42c8b932b5
Josh Womack <jdwomack@csu.fullerton.edu>
2016-11-15 09:14:32 -0800

    added frequency analysis

--- caesar.py
+++ caesar.py
@@ -14,40 +14,47 @@ def main():
             else:
                 rot = int(sys.argv[2])
                 message = sys.argv[3]
+
             print 'Encrypting...'
             message = encrypt(rot, message)
+            print message
 
         elif mode == "-d":
             message = sys.argv[2]
             print 'Decrypting...'
             message = decrypt(message)
+            print 'All possible plaintext:'
+
+            for text in message:
+                print text
+
+            print
+
         else:
             raise Exception
 
-        print message
 
     except:
         print 'caesar.py unknown arguments'
-        print 'Usage:'
-        print '\tpython caesar.py <-e|-d> <N> <Message>'
-        print '\t-e --> encrypt'
-        print '\t-d --> decrypt'
-        print '\t N --> Positive ingeger specifying how much to rotate by'
-        print '\tMessage --> Plaintext to encrypt'
+        print 'Usage: python caesar.py <-e|-d> <N> <Message>'
+        print '  <-e>\t\tencrypt'
+        print '  <-d>\t\tdecrypt'
+        print '  <N>\t\tPositive ingeger specifying how much to rotate by'
+        print '  <Message>\tPlaintext to encrypt\n'
         exit(-1)
 
 
 def encrypt(rot, plaintext):
     message = ""
     for char in plaintext:
-        if ord(char) > 0x41 and ord(char) < 0x5a:
+        if 0x40 < ord(char) < 0x5b:
             # Uppercase
             newval = ord(char) + rot
             if newval > 0x5a:
                 newval = newval - 26
             char = chr(newval)
 
-        elif ord(char) > 0x61 and ord(char) < 0x7a:
+        elif 0x60 < ord(char) < 0x7b:
             # Lowercase
             newval = ord(char) + rot
             if newval > 0x7a:
@@ -60,9 +67,10 @@ def encrypt(rot, plaintext):
 
 
 def decrypt(ciphertext):
-    for i in range(1, 26):
-        print encrypt(i, ciphertext)
-    return None
+    tempList = []
+    for i in range(1, 27):
+        tempList.append(encrypt(i, ciphertext))
+    return tempList
 
 if __name__ == "__main__":
     main()
--- frequency.py
+++ frequency.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python2
 
+import sys
+
 
 def main():
+    ciphertext = ''
+    freqdict = {}
+
+    try:
+        for i in range(1, len(sys.argv)):
+            ciphertext += sys.argv[i]
+
+        ciphertext = ciphertext.lower()
+
+        for i in range(len(ciphertext)):
+            freqdict[ord(ciphertext[i])] = freqdict.get(ord(ciphertext[i]), 0) + 1
+
+        for key, value in freqdict.items():
+            print '{}: {}'.format(chr(key), value)
 
 
+    except:
+        print 'frequency.py unknown arguments'
+        print 'Usage:'
+        print '\tpython frequency.py <Message>'
+        print '\t<Message> --> Plaintext to encrypt'
+        exit(-1)
 
 
 if __name__ == "__main__":
